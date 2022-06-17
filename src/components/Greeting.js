a3190c32cdc408640b97b4f95cb5ae08ba2bec9e
Kth <vcro12123@naver.com>
2022-06-17 19:02:07 +0900

    QA

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
 
-const Greeting = () => {
+const Greeting = (props) => {
+  const [changedText, setChangedText] = useState(false);
+  const changedTextHandler = () => {
+    setChangedText(true);
+  };
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>Is`s good to see you!</p>
+      {!changedText && <p>Is`s good to see you!</p>}
+      {changedText && <p>Changed!</p>}
+      <button onClick={changedTextHandler}>Change Text!</button>
     </div>
   );
 };
